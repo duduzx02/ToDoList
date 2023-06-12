f00f07bc770642e2b87d8850711e81f4da8c2f37
duduzx02 <duduzx02@gmail.com>
2023-06-12 03:12:34 -0300

    inicinado projeto

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sitema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p className='text'>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>✅</button>
+              <button>❌</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App
