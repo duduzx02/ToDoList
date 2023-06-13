e3d3a48e4071295d800e013d29f0fc4b58b30155
duduzx02 <duduzx02@gmail.com>
2023-06-13 20:44:41 -0300

    Manipulando dados de formulário

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState("")
+  const [category, setCategory] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value || !category) return;
+    setValue("")
+    setCategory("")
+    console.log(value, category)
+  } 
+
   return (
     <div className='todo-form'>
       <h2>Criar terefa:</h2>
-      <form>
-        <input type="text" placeholder='Digite o título'/>
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} placeholder='Digite o título' onChange={(e) => setValue(e.target.value)}/>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecionar uma categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
