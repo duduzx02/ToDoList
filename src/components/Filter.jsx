17c2301fe398ec2b475bc00eaa3da773b02ede41
duduzx02 <duduzx02@gmail.com>
2023-06-14 01:17:24 -0300

    Criando funcionalidade de ordenação

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,4 @@
-const Filter = ({filter, setFilter}) => {
+const Filter = ({filter, setFilter, setSort}) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
@@ -13,12 +13,11 @@ const Filter = ({filter, setFilter}) => {
         </div>
         <div>
           <p>Ordem alfabética: </p>
-          <button>Asc</button>
-          <button>Desc</button>
+          <button onClick={() => setSort("Asc")}>Asc</button>
+          <button onClick={() => setSort("Desc")}>Desc</button>
         </div>
       </div>
     </div>
   )
 }
-
 export default Filter
