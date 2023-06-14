94953987dddf14e26c5db13923c402a4a9971cec
duduzx02 <duduzx02@gmail.com>
2023-06-13 21:04:22 -0300

    Adicionando tarefas a lista

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,19 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) =>{
+    const newTodos = [
+      ...todos, {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      }
+    ]
+
+    setTodos(newTodos) 
+  }
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
@@ -36,7 +49,7 @@ function App() {
           <Todo key={todo.id} todo={todo} />
         ))}
       </div>
-      <TodoForm/>
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
